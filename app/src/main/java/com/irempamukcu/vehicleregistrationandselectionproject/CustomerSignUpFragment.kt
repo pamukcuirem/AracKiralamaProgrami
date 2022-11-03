d06ee2490abf6cf06f114b6d91d2b655e66e6c67
pamukcuirem <irempamukcu1@gmail.com>
2022-11-03 23:25:50 +0300

    Ikinci teslim

--- app/src/main/java/com/irempamukcu/vehicleregistrationandselectionproject/CustomerSignUpFragment.kt
+++ app/src/main/java/com/irempamukcu/vehicleregistrationandselectionproject/CustomerSignUpFragment.kt
@@ -1,18 +1,29 @@
 package com.irempamukcu.vehicleregistrationandselectionproject
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.Intent.ACTION_PICK
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_customer_sign_up.*
 
 class CustomerSignUpFragment : Fragment() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,31 +41,71 @@ class CustomerSignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         customerCompleteSignUpButton.setOnClickListener {
             musteriKayit(it)
+
+        }
+
+        addIdentityButton.setOnClickListener {
             addIdentity(it)
+        }
+
+        addDrivingLicenseButton.setOnClickListener {
             addDrivingLicense(it)
         }
         super.onViewCreated(view, savedInstanceState)
     }
 
     fun musteriKayit(view : View){
+        val customerName = customerSignUpNameEditText.text.toString()
+        val customerSurname = customerSignUpSurnameEditText.text.toString()
+        val customerPhoneNumber = customerSignUpPhoneNumberEditText.text.toString()
 
+        if(customerName.length > 15){
+            context?.let {
+                Toast.makeText(it,"İsim 15 karakterden fazla olamaz", Toast.LENGTH_LONG).show()
+            }
+        }
+        if(customerSurname.length > 15){
+            context?.let {
+                Toast.makeText(it,"Soyisim 15 karakterden fazla olamaz",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        if(customerPhoneNumber. length > 11){
+            context?.let {
+                Toast.makeText(it,"Telefon numarası 11 rakamdan fazla olamaz",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        if(customerName.length <= 15 && customerSurname.length <= 15 && customerPhoneNumber. length <= 11){
+            try{
+               context?.let {
+                   val database2 = it.openOrCreateDatabase("Musteriler",Context.MODE_PRIVATE,null)
+                   database2.execSQL("CREATE TABLE IF NOT EXISTS musteriler(id INTEGER PRIMARY KEY, musterisim VARCHAR, musterisoyisim VARCHAR, musterinumara VARCHAR)")
+                   val sqlString2 = "INSERT INTO musteriler(musterisim, musterisoyisim, musterinumara) VALUES (?,?,?)"
+                   val statement2 = database2.compileStatement(sqlString2)
+                   statement2.bindString(1,customerName)
+                   statement2.bindString(2,customerSurname)
+                   statement2.bindString(3,customerPhoneNumber)
+                   statement2.execute()
+               }
+            }catch(e : Exception){
+                e.printStackTrace()
+            }
+        }
     }
 
-    fun addIdentity(view: View){
-        activity?.let {
-            if(ContextCompat.checkSelfPermission(it.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
-        } else{
-            val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                startActivityForResult(galeriIntent,2)
-        }
-        }
+    fun addIdentity(view: View) {
 
 
     }
 
     fun addDrivingLicense(view: View){
 
+
     }
+
+
+
+
 
 }
--- app/src/main/java/com/irempamukcu/vehicleregistrationandselectionproject/VehicleAddFragment.kt
+++ app/src/main/java/com/irempamukcu/vehicleregistrationandselectionproject/VehicleAddFragment.kt
@@ -1,14 +1,29 @@
 package com.irempamukcu.vehicleregistrationandselectionproject
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_PICK
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_vehicle_add.*
+import java.io.ByteArrayOutputStream
 
 class VehicleAddFragment : Fragment() {
+
+    var vehicleImage : Uri? = null
+    var vehicleBitmap : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,17 +41,24 @@ class VehicleAddFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         vehicleSaveButton.setOnClickListener {
             saveVehicle(it)
+
         }
+
+        addVehicleImage.setOnClickListener {
+            saveVehiclePicture(it)
+        }
+
+
         super.onViewCreated(view, savedInstanceState)
     }
     fun saveVehicle(view : View){
         val vehicleModel = addVehicleModelEditText.text.toString()
         val licensePlate = addVehicleLicensePlateEditText.text.toString()
         val vehicleHealth = addVehicleHealthEditText.text.toString()
-        val vehiclePower = addVehiclePowerEditText.text.toString().toInt()
+        val vehiclePower = addVehiclePowerEditText.text.toString()
         val gearType = addVehicleGearTypeEditText.text.toString()
-        val howManyPerson = addVehicleHowManyPersonEditText.text.toString().toInt()
-        val dailyPrice = addVehicleDailyPriceEditText.text.toString().toInt()
+        val howManyPerson = addVehicleHowManyPersonEditText.text.toString()
+        val dailyPrice = addVehicleDailyPriceEditText.text.toString()
 
 
         if(vehicleModel.length > 30){
@@ -57,7 +79,7 @@ class VehicleAddFragment : Fragment() {
             }
         }
 
-        if(vehiclePower > 9999){
+        if(vehiclePower.length > 4){
             context?.let {
                 Toast.makeText(it,"Araç beygiri 4 rakamdan fazla olamaz",Toast.LENGTH_LONG).show()
             }
@@ -69,20 +91,94 @@ class VehicleAddFragment : Fragment() {
             }
         }
 
-        if(howManyPerson > 99){
+        if(howManyPerson.length > 2){
             context?.let {
                 Toast.makeText(it,"Kişi sayısı 2 rakamdan fazla olamaz",Toast.LENGTH_LONG).show()
             }
         }
 
-        if(dailyPrice > 999999){
+        if(dailyPrice.length > 8){
             context?.let {
                 Toast.makeText(it,"Günlük ücret 8 rakamdan fazla olamaz",Toast.LENGTH_LONG).show()
             }
         }
 
+        if(vehicleModel.length <= 30 && licensePlate.length <= 11 && ((vehicleHealth.equals("iyi")) || (vehicleHealth.equals("kötü"))) && vehiclePower.length <= 4 && ((gearType.equals("manuel")) || (gearType.equals("otomatik"))) && howManyPerson.length <= 2 && dailyPrice.length <= 8 && vehicleBitmap != null){
 
+            val outputStream = ByteArrayOutputStream()
+            vehicleBitmap!!.compress(Bitmap.CompressFormat.PNG,50,outputStream)
+            val vehicleByte = outputStream.toByteArray()
 
+            try{
+                context?.let{
+                    val database = it.openOrCreateDatabase("Araclar", Context.MODE_PRIVATE,null)
+                    database.execSQL("CREATE TABLE IF NOT EXISTS araclar(id INTEGER PRIMARY KEY, aracmodeli VARCHAR, plaka VARCHAR, saglik VARCHAR, beygir VARCHAR, vitestipi VARCHAR, kackisilik VARCHAR, gunlukucret VARCHAR, aracfoto BLOB)")
+                    val sqlString = "INSERT INTO araclar(aracmodeli, plaka, saglik, beygir, vitestipi, kackisilik, gunlukucret, aracfoto) VALUES (?,?,?,?,?,?,?,?)"
+                    val statement = database.compileStatement(sqlString)
+                    statement.bindString(1,vehicleModel)
+                    statement.bindString(2,licensePlate)
+                    statement.bindString(3,vehicleHealth)
+                    statement.bindString(4,vehiclePower)
+                    statement.bindString(5,gearType)
+                    statement.bindString(6,howManyPerson)
+                    statement.bindString(7,dailyPrice)
+                    statement.bindBlob(8,vehicleByte)
+                    statement.execute()
 
-}
+                }
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+
+        }
+
+    }
+
+    fun saveVehiclePicture(view : View){
+        activity?.let{
+            if(ContextCompat.checkSelfPermission(it.applicationContext,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val vehicleImageIntent = Intent(ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(vehicleImageIntent,2)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults.size >= 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val vehicleImageIntent = Intent(ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(vehicleImageIntent,2)
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 2 && resultCode == Activity.RESULT_OK && data != null){
+            vehicleImage = data.data
+
+            try{
+                context?.let{
+                    if(vehicleImage != null){
+                        if(Build.VERSION.SDK_INT >= 28){
+                            val vehiclesource = ImageDecoder.createSource(it.contentResolver,vehicleImage!!)
+                            vehicleBitmap = ImageDecoder.decodeBitmap(vehiclesource)
+                            addVehicleImage.setImageBitmap(vehicleBitmap)
+                        }else{
+                            vehicleBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver,vehicleImage)
+                            addVehicleImage.setImageBitmap(vehicleBitmap)
+                        }
+                    }
+                }
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
